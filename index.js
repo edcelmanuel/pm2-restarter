3b5c2037922b90557c2741f54d882e81465fd795
JOHN CARL EDCEL A MANUEL <82397145+edcelmanuel@users.noreply.github.com>
2022-02-02 20:09:44 +0800

    .

--- index.js
+++ index.js
@@ -15,7 +15,10 @@
     pm2.restart(9, function() {});
     pm2.restart(10, function() {});
     pm2.restart(11, function() {});
-
+    pm2.restart(12, function() {});
+    pm2.restart(13, function() {});
+    pm2.restart(14, function() {});
+    pm2.restart(15, function() {});
     setTimeout(worker, 60 * 5000);
     }, 60 * 5000);
   });
